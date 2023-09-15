2c833739e1818f21dccf6705a1f2a0060d3611bf
Keith Flynn <flynn.keith@gmail.com>
2023-09-15 17:19:20 -0400

    added if/else to numberPress function

--- Calculator/app/calculator.js
+++ Calculator/app/calculator.js
@@ -1,11 +1,15 @@
 'use sctrict';
-let display = '';
+let display = '0';
 
 /* take input
 calculate
 display
  */
 function numberPress(button) {
-  display = button.innerHTML;
+  if (display === '0') {
+    display = button.innerHTML;
+  } else {
+    display += button.innerHTML;
+  }
   alert(`button text: ${display}`)
 }
