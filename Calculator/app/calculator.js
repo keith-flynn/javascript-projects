ba613f983bdcb67f0a2e16a29cd0a5dcd41dc691
Keith Flynn <flynn.keith@gmail.com>
2023-09-15 21:47:44 -0400

    Add operator functionality
    Make the display string append the mathematical operator.

--- Calculator/app/calculator.js
+++ Calculator/app/calculator.js
@@ -5,11 +5,23 @@ let display = '0';
 calculate
 display
  */
+
+// Take numerical input
 function numberPress(button) {
   if (display === '0') {
+    //if button press is first input
     display = button.innerHTML;
   } else {
+    //else pressing multiple buttons for larger numbers
     display += button.innerHTML;
   }
-  alert(`button text: ${display}`)
+  // DEBUG
+  alert(display)
+}
+
+// Add calculation to I'm not sure yet
+// DEBUG let firstNumber = new String(display);
+function operatorPress(button) {
+  display += ` ${button.innerHTML}`;
+  alert(display);
 }
